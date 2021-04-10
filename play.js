34878da8c6fa2450f84dacfd9ea9be9219426b04
Austin <sicardaustin97@gmail.com>
2021-04-10 03:43:49 +0000

    Added input file, exported setupInput in the input file as a object. Added require to play.js for setupInput to be able to be called on line 7.

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 //play.js
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  // on any input from stdin (standard input), output a "." to stdout
 
-  process.stdin.on('data', handleUserInput);
-  return stdin;
-};
-const handleUserInput = (key) => {
-  if (key === '\u0003') { // \u0003 maps to ctrl+c input
-    process.exit();
-  }
-};
 connect();
 setupInput();
